fe4ff66e4561fcfad73e48d720596251ff047c45
PieMyth <superplayerjoe@comcast.net>
2019-04-15 13:58:04 -0700

    Added more list functionality, can now add with a contact flass, rearrange (reorder) items within the list, insert at a spefic point (will insert at the end if larger than the length of the list), remove an entry at a desired index (will return a nonsense contact class if over the size of the list. Also provided testing for the newly added features.

--- src/list.rs
+++ src/list.rs
@@ -39,6 +39,33 @@ impl List {
         let c = contact::Contact::create(fname, mname, lname, symbol);
         self.contacts.push(c);
     }
+
+    pub fn addc(&mut self, c: contact::Contact) {
+        self.contacts.push(c);
+    }
+
+    pub fn re_order(&mut self, current_index: usize, new_index: usize) {
+        let c = self.contacts.remove(current_index);
+        self.contacts.insert(new_index, c);
+    }
+
+    pub fn insert(&mut self, desired_index: usize, c: contact::Contact) {
+        if self.contacts.len() < desired_index {
+            self.addc(c);
+        }
+        else {
+            self.contacts.insert(desired_index, c);
+        }
+    }
+
+    pub fn remove(&mut self, desired_index: usize) -> contact::Contact{
+        if self.contacts.len() <= desired_index {
+            contact::Contact::create(0,0,0,'a')
+        }
+        else {
+            self.contacts.remove(desired_index)
+        }
+    }
 }
 
 #[test]
@@ -72,4 +99,52 @@ fn add_test() {
     assert_ne!(x,y);
     x.add(1, 2, 3, 'a');
     assert_eq!(x,y);
+}
+
+#[test]
+fn addc_test() {
+    let mut x = List::new();
+    let c = contact::Contact::create(1,2,3,'a');
+    x.addc(c.clone());
+    assert_eq!(x.contacts.remove(0), c);
+}
+
+#[test]
+fn re_order_test() {
+    let mut x = List::new();
+    let c = contact::Contact::create(1,2,3,'a');
+    x.contacts.push(c.clone());
+    x.add(1, 2, 3, 'b');
+    x.add(1, 2, 3, 'c');
+    x.add(1, 2, 3, 'd');
+    x.re_order(0, 3);
+    let c = x.contacts.remove(3);
+    assert_eq!(c, contact::Contact::create(1, 2, 3, 'a'));
+    x.contacts.insert(3, c);
+    assert_eq!(x.contacts.remove(3), contact::Contact::create(1, 2, 3, 'a'));
+}
+
+#[test]
+fn insert_test() {
+    let mut x = List::new();
+    let c = contact::Contact::create(1,2,3,'a');
+    x.insert(100, c.clone());
+    assert_eq!(x.contacts.remove(0), c);
+    x.addc(c);
+    let c = contact::Contact::create(1,2,3,'b');
+    x.insert(0, c.clone());
+    assert_eq!(x.contacts.remove(0), c);
+}
+
+#[test]
+fn remove_test() {
+    let mut x = List::new();
+    assert_eq!(x.remove(0), contact::Contact::create(0, 0, 0, 'a'));
+    let c = contact::Contact::create(1,2,3,'a');
+    x.addc(c.clone());
+    assert_eq!(x.remove(0), c);
+    x.addc(c.clone());
+    x.addc(c.clone());
+    assert_eq!(x.remove(2), contact::Contact::create(0, 0, 0, 'a'));
+    assert_eq!(x.remove(0), c);
 }
